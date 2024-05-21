d74c175a542701423634f988f8123d9aca8cb456
Stefan Heule <stefanheule@gmail.com>
2024-05-20 22:38:19 -0700

    work in progress

--- server/src/storage.ts
+++ server/src/storage.ts
@@ -4,10 +4,14 @@ import { isLocal } from './common/error-reporting';
 import { StoredCallSchedules } from './shared/types';
 import { assertStoredCallSchedules } from './shared/check-type.generated';
 
-export let STORAGE_LOCATION = 'storage.json';
-if (isLocal()) {
-  STORAGE_LOCATION = path.resolve(`${__dirname}/../../storage.json`);
-}
+export const STORAGE_LOCATION_LOCAL = path.resolve(
+  `${__dirname}/../../data/storage/storage.json`,
+);
+export const STORAGE_LOCATION_REMOTE =
+  '/home/stefan/www-live/call-schedule-data/storage/storage.json';
+export const STORAGE_LOCATION = isLocal()
+  ? STORAGE_LOCATION_LOCAL
+  : STORAGE_LOCATION_REMOTE;
 
 export function storeStorage(storage: StoredCallSchedules): void {
   fs.writeFileSync(STORAGE_LOCATION, JSON.stringify(storage, null, 2));
