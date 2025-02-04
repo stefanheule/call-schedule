1c99990d6c10651937d603f59e5cb33defd7011e
Stefan Heule <stefanheule@gmail.com>
2025-02-03 16:17:34 -0800

    work in progress

--- pm2.prod-config.js
+++ pm2.prod-config.js
@@ -6,18 +6,22 @@ module.exports = {
       name: 'cs-server-public',
       script:
         'IS_PROD=yes CALL_SCHEDULE_PUBLIC=yes node -r tsconfig-paths/register server/build/server-private.js',
+      cwd: path.resolve(__dirname, 'server'),
       log_date_format: 'YYYY-MM-DD HH:mm:ss',
       env: {
         TS_NODE_PROJECT: path.resolve(__dirname, 'server/tsconfig.json'),
+        NODE_PATH: path.resolve(__dirname, 'server/build'),
       },
     },
     {
       name: 'cs-server-private',
       script:
         'IS_PROD=yes CALL_SCHEDULE_PUBLIC=no node -r tsconfig-paths/register server/build/server-private.js',
+      cwd: path.resolve(__dirname, 'server'),
       log_date_format: 'YYYY-MM-DD HH:mm:ss',
       env: {
         TS_NODE_PROJECT: path.resolve(__dirname, 'server/tsconfig.json'),
+        NODE_PATH: path.resolve(__dirname, 'server/build'),
       },
     },
   ],
