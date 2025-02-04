0370daf72534441649e53b106618354d68188ba3
Stefan Heule <stefanheule@gmail.com>
2025-02-03 16:01:13 -0800

    work in progress

--- pm2.prod-config.js
+++ pm2.prod-config.js
@@ -3,13 +3,13 @@ module.exports = {
     {
       name: 'cs-server-public',
       script:
-        'IS_PROD=yes CALL_SCHEDULE_PUBLIC=yes node server/build/server-private.js',
+        'IS_PROD=yes CALL_SCHEDULE_PUBLIC=yes node -r tsconfig-paths/register server/build/server-private.js',
       log_date_format: 'YYYY-MM-DD HH:mm:ss',
     },
     {
       name: 'cs-server-private',
       script:
-        'IS_PROD=yes CALL_SCHEDULE_PUBLIC=no node server/build/server-private.js',
+        'IS_PROD=yes CALL_SCHEDULE_PUBLIC=no node -r tsconfig-paths/register server/build/server-private.js',
       log_date_format: 'YYYY-MM-DD HH:mm:ss',
     },
   ],
